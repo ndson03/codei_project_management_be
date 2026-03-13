79949223934bb5e48ba1e07ee519695f0c4c2269
ndson03 <ndson03@gmail.com>
2026-03-13 22:47:40 +0700

    init: create project

---
+++ src/test/java/dpp/codei_project_management_be/CodeiProjectManagementBeApplicationTests.java
@@ -0,0 +1,13 @@
+package dpp.codei_project_management_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeiProjectManagementBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
